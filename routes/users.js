4f1e115b8f9583d54f386d3500051cafe76f53b5
Pilar890 <pilarvillarreal546@gmail.com>
2023-04-06 12:21:55 -0300

    controladores

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
 import express from 'express'
-let router = express.Router()
+import controller from '../controllers/auth/auth.js'
+import controllerVerify from '../controllers/auth/verify.js'
+import validator from '../middlewares/validator.js'
+// import schemaSignUp from '../schemas/usersSignUp.js'
+// import schemaSignIn from '../schemas/usersSignIn.js'
+import accountExistsSignUp from '../middlewares/accountExistsSignUp.js'
+import accountExistsSignIn from '../middlewares/accountExistsSignIn.js'
+import accountHasBeenVerified from '../middlewares/accountHasBeenVerified.js'
+import passwordIsOk from '../middlewares/passwordIsOk.js'
+import passport from '../middlewares/passport.js'
+// import authorRole from '../controllers/auth/update.js'
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+const { verify } = controllerVerify
+const { sign_up,sign_in,sign_out,token } = controller
+// const {update} = authorRole
+let router = express.Router();
+
+router.post('/signup', validator(schemaSignUp), accountExistsSignUp, sign_up)
+router.post('/signin', validator(schemaSignIn), accountExistsSignIn, accountHasBeenVerified, passwordIsOk,sign_in)
+router.post('/token', passport.authenticate('jwt', {session:false}), token)
+router.post('/signout', passport.authenticate('jwt', {session:false}), sign_out)
+router.get('/verify/:verify_code', verify )
+// router.put('/role/author/:id', update )
 
 export default router
