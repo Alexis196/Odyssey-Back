1c660baa0ce4bece32230a76a81b67ce5b28857f
Alexis Franco <ronaldofranco196@gmail.com>
2023-04-07 14:06:31 -0300

    destoy

--- routes/destino.js
+++ routes/destino.js
@@ -1,13 +1,17 @@
 import express from "express";
 import destinos from "../controllers/destinos/create.js";
 import oneDest from '../controllers/destinos/get_one.js'
+import destroyD from "../controllers/destinos/destroy.js";
 
 let router = express.Router()
 const {create} = destinos
 const {getOne} = oneDest
+const {destroy} = destroyD
 
 router.post('/', create)
 
 router.get('/:id', getOne)
+
+router.delete('/:id', destroy)
 
 export default router
