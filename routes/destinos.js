01f6e7e5fd8af28f64465a274bc59eb6dcccbb77
Alexis Franco <ronaldofranco196@gmail.com>
2023-04-07 18:32:42 -0300

    middlewares

--- routes/destinos.js
+++ routes/destinos.js
@@ -4,6 +4,11 @@ import oneDest from '../controllers/destinos/get_one.js'
 import destroyD from "../controllers/destinos/destroy.js";
 import updateD from '../controllers/destinos/update.js'
 import todos from '../controllers/destinos/get_destino.js'
+import finds_id from "../middlewares/auth/finds_id.js";
+import exist_destino from '../middlewares/destinos/exist_destino.js'
+import passport from '../middlewares/passport.js'
+import is_property_of from "../middlewares/sellers/is_property_of.js";
+import is_active from "../middlewares/sellers/is_active.js";
 
 let router = express.Router()
 const {create} = destinos
@@ -12,13 +17,13 @@ const {destroy} = destroyD
 const {update} = updateD
 const {read} = todos
 
-router.post('/', create)
+router.post('/',  passport.authenticate("jwt", { session: false }), exist_destino, finds_id, create)
 
-router.get('/', read)
-router.get('/:id', getOne)
+router.get('/', passport.authenticate('jwt', { session: false }), read)
+router.get('/:id', passport.authenticate('jwt', { session: false }), getOne)
 
-router.put('/:id', update)
+router.put('/:id', passport.authenticate('jwt', { session: false }), update)
 
-router.delete('/:id', destroy)
+router.delete('/:id', passport.authenticate('jwt', { session: false }), finds_id, is_active, is_property_of, destroy)
 
 export default router
