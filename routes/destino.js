25ce5276cec949a3ccd220755953b20384e09596
Alexis Franco <ronaldofranco196@gmail.com>
2023-04-07 14:12:28 -0300

    edit destino

--- routes/destino.js
+++ routes/destino.js
@@ -2,15 +2,19 @@ import express from "express";
 import destinos from "../controllers/destinos/create.js";
 import oneDest from '../controllers/destinos/get_one.js'
 import destroyD from "../controllers/destinos/destroy.js";
+import updateD from '../controllers/destinos/update.js'
 
 let router = express.Router()
 const {create} = destinos
 const {getOne} = oneDest
 const {destroy} = destroyD
+const {update} = updateD
 
 router.post('/', create)
 
 router.get('/:id', getOne)
+
+router.put('/:id', update)
 
 router.delete('/:id', destroy)
 
