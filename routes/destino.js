20f457d11ccc297542573214076dacc911eab36a
Alexis Franco <ronaldofranco196@gmail.com>
2023-04-07 14:20:42 -0300

    traer destinos paginados de a 6

--- routes/destino.js
+++ routes/destino.js
@@ -3,15 +3,18 @@ import destinos from "../controllers/destinos/create.js";
 import oneDest from '../controllers/destinos/get_one.js'
 import destroyD from "../controllers/destinos/destroy.js";
 import updateD from '../controllers/destinos/update.js'
+import todos from '../controllers/destinos/get_destino.js'
 
 let router = express.Router()
 const {create} = destinos
 const {getOne} = oneDest
 const {destroy} = destroyD
 const {update} = updateD
+const {read} = todos
 
 router.post('/', create)
 
+router.get('/', read)
 router.get('/:id', getOne)
 
 router.put('/:id', update)
