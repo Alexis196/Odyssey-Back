b71cf1d562e04264c40b8af6a4004bacf9c31d10
Pilar890 <pilarvillarreal546@gmail.com>
2023-04-12 22:47:43 -0300

    mercadopago

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ import destino from './destinos.js'
 import detail from './details.js';
 import seller from './seller.js'
 import consulta from './consultas.js'
+import compra from './compras.js'
 
 
 import express from 'express'
@@ -17,6 +18,7 @@ router.use('/users', userRouter)
 router.use('/destinos', destino)
 router.use('/details', detail)
 router.use('/seller', seller)
+router.use("/buy", compra);
 router.use('/consulta', consulta )
 
 export default router
