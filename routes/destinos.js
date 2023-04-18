6ab8d5d81c1b238899b6e3305c47366b3c5fa3fc
Alexis Franco <108986498+Alexis196@users.noreply.github.com>
2023-04-18 02:21:21 -0300

    Dev ronal (#4)
    * rama
    
    * dateBase, controller signUp, model
    
    * controladores
    
    * Committing changes before merging remote branch
    
    * models, base de datos sin terminar
    
    * creación de un nuevo destino funcionando
    
    * traer destino por id funcionando
    
    * edicion data
    
    * destoy
    
    * edit destino
    
    * traer destinos paginados de a 6
    
    * create y destroy detail
    
    * detailDestino completo
    
    * creador de seller
    
    * fix
    
    * fix
    
    * middleware sellers
    
    * middlewares
    
    * nombres
    
    * schemas
    
    * agregue CORS en APP
    
    * ruta de verificacion cambiada
    
    * fix data
    
    * alex
    
    * add destinos
    
    * fix joi
    
    * coment
    
    * data>destinos actualizado all 78%
    
    * cambios
    
    * destinos actualizado
    
    * tls email
    
    * name agregado al schema
    
    * modificacion joi
    
    * consultas de los usuarios funcionando
    
    * pull correo
    
    * fix
    
    * packages
    
    * conflict
    
    * mercadopago
    
    * pili
    
    * agregue mercado pago
    
    * cambios para que funcione el dormulario de crear
    
    * api actualization
    
    * rooms and hotels
    
    * get one seller
    
    * traer un usuario
    
    * fix
    
    * host
    
    * category
    
    ---------
    
    Co-authored-by: Pilar890 <pilarvillarreal546@gmail.com>
    Co-authored-by: Juan Rivera <indiojuan2012.jr@gmail.com>
    Co-authored-by: Agustin <pereyra.agus@outlook.com.ar>

--- routes/destinos.js
+++ routes/destinos.js
@@ -11,6 +11,7 @@ import is_property_of from "../middlewares/sellers/is_property_of.js";
 import is_active from "../middlewares/sellers/is_active.js";
 import destinoSchemas from '../schemas/destino.js'
 import validator from '../middlewares/validator.js'
+import controllerCategory from '../controllers/category/category.js'
 
 let router = express.Router()
 const { create } = destinos
@@ -18,14 +19,16 @@ const { getOne } = oneDest
 const { destroy } = destroyD
 const { update } = updateD
 const { read } = todos
+const { show } = controllerCategory
 
 router.post('/', passport.authenticate("jwt", { session: false }), validator(destinoSchemas), exist_destino, finds_id, create)
 
+router.get('/categories', show)
 router.get('/', passport.authenticate('jwt', { session: false }), read)
 router.get('/:id', passport.authenticate('jwt', { session: false }), getOne)
 
 router.put('/:id', passport.authenticate('jwt', { session: false }), update)
 
-router.delete('/:id', passport.authenticate('jwt', { session: false }), finds_id, is_active, is_property_of, destroy)
+router.delete('/:id', passport.authenticate('jwt', { session: false }), finds_id, is_active, destroy)
 
 export default router
